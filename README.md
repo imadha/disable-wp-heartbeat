ab46a4cdca79c0137a5166d6ce3c62eacc61c571
Ibrahim Madha <imadha@Ibus-MacBook-Air.local>
2014-05-07 17:44:15 -0700

    Adding the files and editing the README.

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ disable-wp-heartbeat
 
 A dead-simple WordPress plugin to disable the Heartbeat API and autosave for the all admin screens
 
-see this post here for why you might want to do this: http://ibrahim.madha.net/how-to-disable-the-heartbeat-api-on-wordpress-3-6/
+Simply move the disable-wp-heartbeat directory into your plugins directory and turn it on.  Beware, several plugins and core features rely on Heartbeat.  Use this at your own risk.
+ 
+See this post here for why you might want to do this: http://ibrahim.madha.net/how-to-disable-the-heartbeat-api-on-wordpress-3-6/
---
+++ disable-wp-heartbeat/disable-wp-heartbeat.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name: Disable WordPress Heartbeat
+ * Plugin URI: https://github.com/imadha/disable-wp-heartbeat
+ * Description: Disables Heartbeat, post locking, auto-saving, and auth checking.  Please only use this if you are absolutely sure you do not want to take advantage of the Heartbeat features, and aren't running any plugins or themes that rely on this feature
+ * Author: Ibrahim Madha
+ * Author URI: http://ibrahim.madha.net/
+ * Version: 0.1
+ */
+
+function disable_wp_heartbeat_init() {
+    wp_deregister_script( 'autosave' );
+	wp_deregister_script( 'heartbeat' );
+    wp_register_script( 'heartbeat', plugin_dir_url(__FILE__)."dummy.js" );
+}
+add_action( 'init', 'disable_wp_heartbeat_init', 1 );
